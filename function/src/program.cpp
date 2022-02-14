df80ee14f7af6e23939cf8455471f8c789a7d9d0
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2022-02-15 01:07:20 +0300

    Ver. 3

--- function/src/program.cpp
+++ function/src/program.cpp
@@ -2,49 +2,20 @@
 
 #include "fonts/DroidSans.h"
 
-#include <imgui_stdlib.h>
-#include <imgui-sfml.h>
-#include <imgui.h>
+#include <fmt/format.h>
 
-#include "fmt/format.h"
+#include <inipp/inipp.h>
 
 #include <fstream>
 #include <sstream>
 
 Program::Program() {
 	window.create(sf::VideoMode(640, 480), L"function");
-	window.setFramerateLimit(30);
 	ImGui::SFML::Init(window, false);
+	ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 	ImGui::StyleColorsLight();
-	loadFonts();
-	loadScript();
-	L = luaL_newstate();
-	luaL_openlibs(L);
-	fmt::print("Initialized.\n");
-}
+	ImGui::SetColorEditOptions(ImGuiColorEditFlags_PickerHueWheel);
 
-Program::~Program() {
-	ImGui::SFML::Shutdown();
-	lua_close(L);
-	saveScript();
-	fmt::print("Shut down.\n");
-}
-
-void Program::saveScript() {
-	std::ofstream file("function.txt");
-	file << luaScript;
-	fmt::print("Script saved.\n");
-}
-
-void Program::loadScript() {
-	std::ifstream file("function.txt");
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-	luaScript = buffer.str();
-	fmt::print("Script loaded.\n");
-}
-
-void Program::loadFonts() {
 	ImGuiIO& io = ImGui::GetIO();
 	ImFontConfig config;
 	config.FontDataOwnedByAtlas = false;
@@ -56,22 +27,97 @@ void Program::loadFonts() {
 	io.Fonts->AddFontFromMemoryTTF((void*)DroidSans_data, DroidSans_size, 16, &config, ranges.Data);
 	font.loadFromMemory(DroidSans_data, DroidSans_size);
 	ImGui::SFML::UpdateFontTexture();
-	fmt::print("Fonts loaded.\n");
+
+	{
+		std::ifstream file("function.txt");
+		if (file.is_open()) {
+			std::stringstream buf;
+			buf << file.rdbuf();
+			luaScript = buf.str();
+		} else {
+			luaScript = "function f(x)\n"
+				"\treturn x\n"
+				"end";
+		}
+	}
+
+	{
+		inipp::Ini<char> ini;
+		std::ifstream file("function.ini");
+		ini.parse(file);
+		unsigned uGraphCol, uBgCol;
+		if (inipp::get_value(ini.sections["Colors"], "GraphColor", uGraphCol)) {
+			graphCol = ImGui::ColorConvertU32ToFloat4(uGraphCol);
+		}
+		if (inipp::get_value(ini.sections["Colors"], "BackgroundColor", uBgCol)) {
+			bgCol = ImGui::ColorConvertU32ToFloat4(uBgCol);
+		}
+
+		if (ini.sections["Rendering"]["FPS"] == "VSync") {
+			fps = 0;
+		} else if (ini.sections["Rendering"]["FPS"] == "Unlocked") {
+			fps = -1;
+		} else {
+			inipp::get_value(ini.sections["Rendering"], "FPS", fps);
+		}
+
+		if (fps == 0) {
+			window.setVerticalSyncEnabled(true);
+			window.setFramerateLimit(0);
+		} else if (fps > 0) {
+			window.setVerticalSyncEnabled(false);
+			window.setFramerateLimit(fps);
+		} else {
+			window.setVerticalSyncEnabled(false);
+			window.setFramerateLimit(0);
+		}
+	}
+
+	L = luaL_newstate();
+	luaL_openlibs(L);
+	fmt::print("Initialized.\n");
+}
+
+Program::~Program() {
+	ImGui::SFML::Shutdown();
+	lua_close(L);
+
+	{
+		std::ofstream file("function.txt");
+		file << luaScript;
+	}
+
+	{
+		inipp::Ini<char> ini;
+		ini.sections["Colors"]["GraphColor"] = fmt::format("{}", ImGui::ColorConvertFloat4ToU32(graphCol));
+		ini.sections["Colors"]["BackgroundColor"] = fmt::format("{}", ImGui::ColorConvertFloat4ToU32(bgCol));
+
+		if (fps == 0) {
+			ini.sections["Rendering"]["FPS"] = "VSync";
+		} else if (fps < 0) {
+			ini.sections["Rendering"]["FPS"] = "Unlocked";
+		} else {
+			ini.sections["Rendering"]["FPS"] = fmt::format("{}", fps);
+		}
+
+		std::ofstream file("function.ini");
+		file << "; Colors are ABGR\n"
+			"; FPS: 'VSync', 'Unlocked' or any value\n";
+		ini.generate(file);
+	}
+
+	fmt::print("Shut down.\n");
 }
 
 void Program::tick() {
 	info_dragging = false;
 	info_hovered = false;
 
+	ImGui::DockSpaceOverViewport(NULL, ImGuiDockNodeFlags_PassthruCentralNode);
+
 	if (ImGui::Begin(u8"График функции")) {
-		sf::Vector2u region(std::max(1.0f, ImGui::GetContentRegionAvail().x),
-							std::max(1.0f, ImGui::GetContentRegionAvail().y));
+		sf::Vector2u region(std::max(sf::Vector2f(1, 1), sf::Vector2f(ImGui::GetContentRegionAvail())));
 		if (graphSurf.getSize() != region) {
-			static bool centered = false;
-			if (!centered) {
-				//view_pos = sf::Vector2f(region / 2u);
-				centered = true;
-			}
 			graphSurf.create(region.x, region.y);
 			fmt::print("Surface resized to ({}; {})\n", region.x, region.y);
 			renderGraph();
@@ -80,10 +126,7 @@ void Program::tick() {
 		if (ImGui::IsItemActive()) {
 			sf::Vector2f off = ImGui::GetIO().MouseDelta;
 			if (off != sf::Vector2f(0, 0)) {
-				view_pos += off;
-				//sf::View view = graphSurf.getView();
-				//view.move(-off);
-				//graphSurf.setView(view);
+				view_offset -= off * view_scale;
 				fmt::print("Panned by ({}; {})\n", off.x, off.y);
 				renderGraph();
 			}
@@ -93,19 +136,11 @@ void Program::tick() {
 			if (ImGui::GetIO().MouseWheel != 0) {
 				if (ImGui::GetIO().MouseWheel > 0) {
 					for (float i = 0; i < ImGui::GetIO().MouseWheel; i += 1.0f) {
-						view_scale.x *= 0.9f;
-						view_scale.y *= 0.9f;
-						//sf::View view = graphSurf.getView();
-						//view.zoom(0.9f);
-						//graphSurf.setView(view);
+						view_scale *= 0.9f;
 					}
 				} else {
 					for (float i = 0; i > ImGui::GetIO().MouseWheel; i -= 1.0f) {
-						view_scale.x *= 1.1f;
-						view_scale.y *= 1.1f;
-						//sf::View view = graphSurf.getView();
-						//view.zoom(1.1f);
-						//graphSurf.setView(view);
+						view_scale *= 1.1f;
 					}
 				}
 				fmt::print("Zoomed by {}\n", ImGui::GetIO().MouseWheel);
@@ -129,12 +164,29 @@ void Program::tick() {
 	}
 	ImGui::End();
 
-	if (ImGui::Begin(u8"Информация")) {
-		ImGui::Text("dragging: %d", info_dragging);
-		ImGui::Text("hovered: %d", info_hovered);
-		ImGui::Text("lua top: %d", lua_gettop(L));
-		ImGui::Text("view pos: (%f; %f)", view_pos.x, view_pos.y);
-		ImGui::Text("view scale: (%f; %f)", view_scale.x, view_scale.y);
+	bool info = false;
+	if (info) {
+		if (ImGui::Begin("info")) {
+			ImGui::Text("dragging: %d", info_dragging);
+			ImGui::Text("hovered: %d", info_hovered);
+			ImGui::Text("lua top: %d", lua_gettop(L));
+			bool rerender = false;
+			rerender |= ImGui::InputFloat2("view offset", &view_offset.x);
+			rerender |= ImGui::InputFloat2("view scale", &view_scale.x);
+			if (rerender) {
+				renderGraph();
+			}
+		}
+		ImGui::End();
+	}
+
+	if (ImGui::Begin(u8"Настройки")) {
+		bool rerender = false;
+		rerender |= ImGui::ColorEdit4(u8"Цвет графика", &graphCol.x, ImGuiColorEditFlags_NoInputs);
+		rerender |= ImGui::ColorEdit4(u8"Цвет заднего фона", &bgCol.x, ImGuiColorEditFlags_NoInputs);
+		if (rerender) {
+			renderGraph();
+		}
 	}
 	ImGui::End();
 
@@ -150,7 +202,15 @@ void Program::renderGraph() {
 		return;
 	}
 
+	// look into how to properly manage lua stack
 	lua_settop(L, 0);
+
+	sf::View view;
+	view.setSize(sf::Vector2f(graphSurf.getSize()) * view_scale);
+	view.setCenter(view_offset);
+	graphSurf.setView(view);
+
+	sf::Vector2f view_pos = view.getCenter() - view.getSize() / 2.0f;
 
 	sf::VertexArray vertices(sf::Points);
 	for (unsigned i = 0; i < graphSurf.getSize().x; i++) {
@@ -160,9 +220,7 @@ void Program::renderGraph() {
 			break;
 		}
 
-		double screen_x = static_cast<double>(i);
-		double fx = screen_x - view_pos.x;
-		fx *= view_scale.x;
+		double fx = view_pos.x + double(i) / double(graphSurf.getSize().x) * view.getSize().x;
 
 		lua_pushnumber(L, fx);
 		if (lua_pcall(L, 1, 1, 0) != LUA_OK) {
@@ -176,30 +234,96 @@ void Program::renderGraph() {
 		}
 
 		double fy = lua_tonumber(L, -1);
-		fy /= view_scale.y;
-		double screen_y = view_pos.y - fy;
-		vertices.append(sf::Vertex(sf::Vector2f(screen_x, screen_y), sf::Color::Black));
+		vertices.append(sf::Vertex(sf::Vector2f(fx, -fy), graphCol));
 	}
 
-	graphSurf.clear(sf::Color::White);
+	graphSurf.clear(bgCol);
 
-	sf::VertexArray axis(sf::Lines);
-	axis.append(sf::Vertex(sf::Vector2f(0, view_pos.y), sf::Color::Black));
-	axis.append(sf::Vertex(sf::Vector2f(graphSurf.getSize().x, view_pos.y), sf::Color::Black));
-	axis.append(sf::Vertex(sf::Vector2f(view_pos.x, 0), sf::Color::Black));
-	axis.append(sf::Vertex(sf::Vector2f(view_pos.x, graphSurf.getSize().y), sf::Color::Black));
-	graphSurf.draw(axis);
+	float xrel = 150.0f * view_scale.x;
+	float xstep = std::max(1000.0f, std::floor_to(xrel, 1000.0f));
+	if (xrel < 1) {
+		xstep = 1;
+	} else if (xrel < 2) {
+		xstep = 2;
+	} else if (xrel < 5) {
+		xstep = 5;
+	} else if (xrel < 10) {
+		xstep = 10;
+	} else if (xrel < 20) {
+		xstep = 20;
+	} else if (xrel < 50) {
+		xstep = 50;
+	} else if (xrel < 100) {
+		xstep = 100;
+	} else if (xrel < 200) {
+		xstep = 200;
+	} else if (xrel < 500) {
+		xstep = 500;
+	}
+
+	float yrel = 150.0f * view_scale.y;
+	float ystep = std::max(1000.0f, std::floor_to(yrel, 1000.0f));
+	if (yrel < 1) {
+		ystep = 1;
+	} else if (yrel < 2) {
+		ystep = 2;
+	} else if (yrel < 5) {
+		ystep = 5;
+	} else if (yrel < 10) {
+		ystep = 10;
+	} else if (yrel < 20) {
+		ystep = 20;
+	} else if (yrel < 50) {
+		ystep = 50;
+	} else if (yrel < 100) {
+		ystep = 100;
+	} else if (yrel < 200) {
+		ystep = 200;
+	} else if (yrel < 500) {
+		ystep = 500;
+	}
+
+	sf::VertexArray axes(sf::Lines);
+	for (float axis_x = std::floor_to(view_pos.x, xstep); axis_x <= view_pos.x + view.getSize().x; axis_x += xstep) {
+		axes.append(sf::Vertex(sf::Vector2f(axis_x, view_pos.y), sf::Color(128, 128, 255, 128)));
+		axes.append(sf::Vertex(sf::Vector2f(axis_x, view_pos.y + view.getSize().y), sf::Color(128, 128, 255, 128)));
+	}
+
+	for (float axis_y = std::floor_to(view_pos.y, ystep); axis_y <= view_pos.y + view.getSize().y; axis_y += ystep) {
+		axes.append(sf::Vertex(sf::Vector2f(view_pos.x, axis_y), sf::Color(128, 128, 255, 128)));
+		axes.append(sf::Vertex(sf::Vector2f(view_pos.x + view.getSize().x, axis_y), sf::Color(128, 128, 255, 128)));
+	}
+
+	graphSurf.draw(axes);
 
 	graphSurf.draw(vertices);
 
-	sf::Text text;
-	text.setFont(font);
-	text.setCharacterSize(12);
-	text.setFillColor(sf::Color::Black);
-	text.setString("0");
-	text.setPosition(view_pos.x, view_pos.y);
-	AlignText(text, HAlign::Right, VAlign::Top, 2);
-	graphSurf.draw(text);
+	for (float label_x = std::floor_to(view_pos.x, xstep); label_x <= view_pos.x + view.getSize().x + 50.0f; label_x += xstep) {
+		sf::Text text;
+		text.setFont(font);
+		text.setCharacterSize(12);
+		text.setFillColor(sf::Color::Black);
+		text.setScale(view_scale);
+		text.setString(fmt::format("{}", label_x));
+		text.setPosition(label_x, 0);
+		AlignText(text, HAlign::Right, VAlign::Top, 2);
+		graphSurf.draw(text);
+	}
+
+	for (float label_y = std::floor_to(view_pos.y, ystep); label_y <= view_pos.y + view.getSize().y + 50.0f; label_y += ystep) {
+		if (label_y == 0) {
+			continue;
+		}
+		sf::Text text;
+		text.setFont(font);
+		text.setCharacterSize(12);
+		text.setFillColor(sf::Color::Black);
+		text.setScale(view_scale);
+		text.setString(fmt::format("{}", -label_y));
+		text.setPosition(0, label_y);
+		AlignText(text, HAlign::Right, VAlign::Top, 2);
+		graphSurf.draw(text);
+	}
 
 	graphSurf.display();
 	fmt::print("Rerendered.\n");
@@ -220,6 +344,12 @@ void Program::run() {
 		CursorWorkaround(window);
 		window.clear();
 		ImGui::SFML::Render(window);
+		if (!window.hasFocus()) {
+			sf::RectangleShape r;
+			r.setFillColor(sf::Color(0, 0, 0, 128));
+			r.setSize(sf::Vector2f(window.getSize()));
+			window.draw(r);
+		}
 		window.display();
 	}
 }
@@ -249,8 +379,7 @@ void AlignText(sf::Text& text, HAlign halign, VAlign valign, float offset) {
 			origin.y = 2.0f * bounds.top + bounds.height + offset;
 			break;
 	}
-	origin.x = std::floor(origin.x);
-	origin.y = std::floor(origin.y);
+	origin = std::floor(origin);
 	text.setOrigin(origin);
 }
 
--- function/src/program.h
+++ function/src/program.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+
+#include <imgui_stdlib.h>
+#include <imgui-sfml.h>
+#include <imgui.h>
+
 #include <lua.hpp>
 
 enum class HAlign { Left, Center, Right };
@@ -11,18 +16,14 @@ public:
 	Program();
 	~Program();
 
-	void run();
-
-	void saveScript();
-	void loadScript();
-	void loadFonts();
 	void tick();
 	void renderGraph();
+	void run();
 
 	sf::RenderWindow window;
 	sf::RenderTexture graphSurf;
-	sf::Vector2f view_pos;
-	sf::Vector2f view_scale = sf::Vector2f(1, 1);
+	sf::Vector2f view_offset = sf::Vector2f(0, 0);
+	sf::Vector2f view_scale = sf::Vector2f(0.02, 0.02);
 	std::string luaScript;
 	std::string errorMsg;
 	sf::Font font;
@@ -30,6 +31,11 @@ public:
 
 	bool info_dragging = false;
 	bool info_hovered = false;
+
+	ImVec4 graphCol = ImVec4(0, 0, 0, 1); // normalized
+	ImVec4 bgCol = ImVec4(1, 1, 1, 1);
+
+	int fps = 30;
 };
 
 void AlignText(sf::Text& text, HAlign halign, VAlign valign, float offset = 0);
@@ -38,3 +44,56 @@ void AlignText(sf::Text& text, HAlign halign, VAlign valign, float offset = 0);
 * https://github.com/eliasdaler/imgui-sfml/issues/119#issuecomment-622322382
 */
 void CursorWorkaround(sf::RenderWindow& window);
+
+namespace std
+{
+	template <typename T>
+	sf::Vector2<T> floor(sf::Vector2<T> v) {
+		return sf::Vector2<T>(floor(v.x), floor(v.y));
+	}
+
+	template <typename T>
+	sf::Vector2<T> round(sf::Vector2<T> v) {
+		return sf::Vector2<T>(round(v.x), round(v.y));
+	}
+
+	template <typename T>
+	sf::Vector2<T> ceil(sf::Vector2<T> v) {
+		return sf::Vector2<T>(ceil(v.x), ceil(v.y));
+	}
+
+	template <typename T>
+	sf::Vector2<T> min(sf::Vector2<T> a, sf::Vector2<T> b) {
+		return sf::Vector2<T>(min(a.x, b.x), min(a.y, b.y));
+	}
+
+	template <typename T>
+	sf::Vector2<T> max(sf::Vector2<T> a, sf::Vector2<T> b) {
+		return sf::Vector2<T>(max(a.x, b.x), max(a.y, b.y));
+	}
+
+	template <typename T>
+	T floor_to(T x, T n) {
+		return floor(x / n) * n;
+	}
+
+	template <typename T>
+	T round_to(T x, T n) {
+		return round(x / n) * n;
+	}
+
+	template <typename T>
+	T ceil_to(T x, T n) {
+		return ceil(x / n) * n;
+	}
+}
+
+template <typename T>
+sf::Vector2<T> operator *(sf::Vector2<T> rhs, sf::Vector2<T> lhs) {
+	return sf::Vector2<T>(rhs.x * lhs.x, rhs.y * lhs.y);
+}
+
+template <typename T>
+sf::Vector2<T> operator /(sf::Vector2<T> rhs, sf::Vector2<T> lhs) {
+	return sf::Vector2<T>(rhs.x / lhs.x, rhs.y / lhs.y);
+}
